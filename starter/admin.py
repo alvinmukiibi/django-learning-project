3c4141638e27b94242eea0050f790ee42e04041d
Alvin Mukiibi <mukikelly792@gmail.com>
2020-01-23 16:10:05 +0300

    make a model and save it to db

--- starter/admin.py
+++ starter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# this is a relative import since admin and models are in the same directory
+from .models import Product
 # Register your models here.
+
+admin.site.register(Product)
--- starter/models.py
+++ starter/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+# we always run makemigrations and migrate whenever we make changes to our models
+
 
 class Product(models.Model):
     title = models.TextField()
     description = models.TextField()
     price = models.TextField()
+    summary = models.TextField(default='this is awesome')
