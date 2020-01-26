44318656038f28ed697c034d8ff0cc3d276e654b
Alvin Mukiibi <mukikelly792@gmail.com>
2020-01-26 12:19:38 +0300

    rendering context in a template

--- mypages/views.py
+++ mypages/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# django takes our template, and the template context, mashes them together, renders it and sends back raw html to the browser
+
 
 def home_view(request, *args, **kwargs):
     print(request.user)  # accessing the authenticated user on the request object
@@ -12,4 +14,9 @@ def home_view(request, *args, **kwargs):
 
 def contact_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Contact page</h1>")
-    return render(request, 'contact.html', {})
+    my_context = {
+        "my_text": "This is a context value being passed in using a dictionary",
+        "my_number": 246,
+        "my_list": [12, 34, 12, 44]
+    }
+    return render(request, 'contact.html', my_context)
