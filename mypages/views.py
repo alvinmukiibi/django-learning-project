e914b1f669e1a0ecff1f565e5dc0a26dd7a8bf37
Alvin Mukiibi <mukikelly792@gmail.com>
2020-01-26 11:48:35 +0300

    templating

--- mypages/views.py
+++ mypages/views.py
@@ -6,8 +6,10 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
     print(request.user)  # accessing the authenticated user on the request object
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'home.html', {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact page</h1>")
+def contact_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Contact page</h1>")
+    return render(request, 'contact.html', {})
