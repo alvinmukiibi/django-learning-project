948f6f131abd27db9a67b2f30db903ce7094c07b
Alvin Mukiibi <mukikelly792@gmail.com>
2020-01-26 10:55:16 +0300

    blank and null

--- starter/models.py
+++ starter/models.py
@@ -7,7 +7,9 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=120)  # max_length is required
+    # blank is for a field being required ie. when its false
+    # null is for the db, whether a field is nullable
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary = models.TextField()
+    summary = models.TextField(blank=False)
     featured = models.BooleanField()  # adding a new field when u already migrated
